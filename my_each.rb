ae33488a38e49369cb4a81a60baa2940f124b6dd
Sheila Wittman <szandora789@gmail.com>
2018-06-08 22:20:53 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -11,17 +11,3 @@ else
   puts "Hey! No block was given!"
 end
 end
-
-
-
-
-def hello_t(array)
-
-
-# call your method here!
-
-#hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
